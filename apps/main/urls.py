26f9038a9eee304e2df36580f9d4e777ffc8107d
D3APC <BioRazor.a@gmail.com>
2016-07-13 21:33:00 -0400

    Agregada funcion de prueba para generar pdfs

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import url, include
 
+from .views import myview
+
 urlpatterns = [
+	url(r'^pdf/', myview, name='pdf')
 ]
--- apps/main/views.py
+++ apps/main/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+import io
+from xhtml2pdf import pisa
+from django.template.loader import get_template
+from django.template import Context
+from django.http import HttpResponse
+from cgi import escape
+
+
+def generarPDF(template_src, context_dict):
+    template = get_template(template_src)
+    context = Context(context_dict)
+    html  = template.render(context)
+    result = io.BytesIO()
+
+    pdf = pisa.pisaDocument(io.BytesIO(html.encode("ISO-8859-1")), result)
+    if not pdf.err:
+        return HttpResponse(result.getvalue(), content_type='application/pdf')
+    return HttpResponse('We had some errors<pre>%s</pre>' % escape(html))
+
+def myview(request):
+	context = {'titulo' : 'Primer PDF generado con Django',
+				'contenido' : 'Generado con python 3  reportlab'
+	}
+	return generarPDF('mytemplate.html', context)
