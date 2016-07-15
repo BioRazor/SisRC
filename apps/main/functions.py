cf4925d2ba8547f57a625b15399b788d224b98c6
D3APC <BioRazor.a@gmail.com>
2016-07-15 10:41:08 -0400

    Añadida la compatibilidad con python 2 para el generador de pdf's

---
+++ apps/main/functions.py
@@ -0,0 +1,28 @@
+#Para python 3
+try:
+	import io
+#para python 2
+except:
+	import cStringIO as StringIO
+
+from xhtml2pdf import pisa
+from django.template.loader import get_template
+from django.template import Context
+from django.http import HttpResponse
+from cgi import escape
+
+def generarPDF(template_src, context_dict):
+	template = get_template(template_src)
+	context = context_dict
+	html  = template.render(context)
+	#para python 3
+	try:
+		result = io.BytesIO()
+		pdf = pisa.pisaDocument(io.BytesIO(html.encode("ISO-8859-1")), result)
+	except:
+		result = StringIO.StringIO()
+		pdf = pias.pisaDocument(StringIO.StringIO(html.encode("ISO-8859-1")), result)
+
+	if not pdf.err:
+		return HttpResponse(result.getvalue(), content_type='application/pdf')
+	return HttpResponse('We had some errors<pre>%s</pre>' % escape(html))
--- apps/main/views.py
+++ apps/main/views.py
@@ -1,23 +1,6 @@
 from django.shortcuts import render
 
-import io
-from xhtml2pdf import pisa
-from django.template.loader import get_template
-from django.template import Context
-from django.http import HttpResponse
-from cgi import escape
-
-
-def generarPDF(template_src, context_dict):
-    template = get_template(template_src)
-    context = Context(context_dict)
-    html  = template.render(context)
-    result = io.BytesIO()
-
-    pdf = pisa.pisaDocument(io.BytesIO(html.encode("ISO-8859-1")), result)
-    if not pdf.err:
-        return HttpResponse(result.getvalue(), content_type='application/pdf')
-    return HttpResponse('We had some errors<pre>%s</pre>' % escape(html))
+from .functions import generarPDF
 
 def myview(request):
 	context = {'titulo' : 'Primer PDF generado con Django',
